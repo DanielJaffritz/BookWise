f1b38d19bed165a72ff2fb1249b956221dfdc58a
DanielDev <danieljaffritz08@gmail.com>
2026-08-15 07:43:26 +0200

    s

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -12,7 +12,7 @@ export default function MyProfile() {
       }}
         className="mb-10"
       >
-        <Button>Logout</Button>
+        <button>Logout</button>
       </form>
       <BookList title="borrowed books" books={sampleBooks} />
     </>
