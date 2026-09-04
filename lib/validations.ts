c285d13ed8d8370beba2683c5a71f7cb0a20a043
DanielDev <danieljaffritz08@gmail.com>
2026-09-04 07:53:20 +0200

    validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 export const signUpSchema = z.object({
   fullName: z.string().min(3),
   email: z.string().email(),
-  universityId: z.coerce.number().max(8),
+  universityId: z.coerce.number().min(8),
   universityCard: z.string().nonempty("university card is required"),
   password: z.string().min(8)
 })
